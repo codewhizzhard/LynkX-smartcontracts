74eae561c1948d3d81663aff951d93f560dcb35c
codewhizzhard <topeadeolu5@gmail.com>
2025-07-04 23:55:28 +0100

    added bance functionality

--- src/Vault.sol
+++ src/Vault.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.19;
 
 contract Vault {
     address public owner;
+
+    mapping(address => uint256) public balance;
     
     function initialise(address _user) external {
         require(owner == address(0), "Vault already initialised");
