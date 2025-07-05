e3eca1f07101355f2192ba19fcec2fcee6d387e8
codewhizzhard <topeadeolu5@gmail.com>
2025-07-05 01:55:52 +0100

    Added the usdc only token to be transferred round the chain, with the transaction logic

--- src/FactoryVault.sol
+++ src/FactoryVault.sol
@@ -19,6 +19,11 @@ contract VaultFactory {
     using Clones for address;
 
     address public immutable vaultImplementation;
+    mapping(uint256 => address) public chainIdToVault;
+
+    chainIdToVault[1] = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;// Ethereum Mainnet USDC
+    chainIdToVault[137] = 0x2791Bca1f25dB3aF4eD8f9eF7bA6c0cBf4bE8fD6;// Polygon USDC 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174
+    chainIdToVault[42161] = 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8; // Arbitrum USDC
 
     constructor(address _vaultImplementation) {
         vaultImplementation = _vaultImplementation;
@@ -39,7 +44,7 @@ contract VaultFactory {
 
         bytes32 salt = keccak256(abi.encodePacked(msg.sender));
         vault = vaultImplementation.cloneDeterministic(salt);
-        IVault(vault).initialise(msg.sender)
+        IVault(vault).initialise(msg.sender, chainIdToVault[chainId])
         userToVaults[msg.sender][chainId] = VaultInfo({
             vaultAddress: address(vault),
             chainId: chainId
--- src/Vault.sol
+++ src/Vault.sol
@@ -2,13 +2,37 @@
 
 pragma solidity 0.8.19;
 
-contract Vault {
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
+contract Vault is {
     address public owner;
 
-    mapping(address => uint256) public balance;
+    IERC20 public usdc;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not Authorized");
+        _;
+    } 
     
-    function initialise(address _user) external {
+    function initialise(address _user, address _usdc) external {
         require(owner == address(0), "Vault already initialised");
         owner = _user;
+        usdc = IERC20(_usdc);
+    }
+
+    function deposit(uint256 _amt) external onlyOwner {
+       require(_amt > 0, "Amount must be greater than Zero");
+       require(usdc.transferFrom(msg.sender, address(this), _amt), "Transfer Failed");
+    }
+
+    function withdraw(uint256 _amt) external onlyOwner {
+        require(_amt > 0, "Amount must be greater than Zero");
+        require(usdc.transfer(msg.sender, _amt), "Transfer Failed");
+       
+    }
+
+    function getBalance() external onlyOwner returns (uint256 balance) {
+        balance = usdc.balanceOf(address(this));
     }
 }
